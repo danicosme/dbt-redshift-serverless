9071e28763d47ac683a3a34ee6a0ac62fc19e42a
Daniele B <danicbeltrao@gmail.com>
2023-08-12 17:25:23 -0300

    Atualizando view de vendas

--- analytics_dbt/models/views/vw_sales.sql
+++ analytics_dbt/models/views/vw_sales.sql
@@ -4,11 +4,16 @@ WITH source_sales as (
 ),
 renamed as (
     SELECT
-         SALESID AS ID_VENDA
-        ,LISTID AS ID_LISTA
-        ,BUYERID AS ID_COMPRADOR
-        ,DATEID AS ID_DATE
-        ,SALETIME AS DATA_VENDA
+         SALESID                                        AS ID_VENDA
+        ,LISTID                                         AS ID_LISTA
+        ,BUYERID                                        AS ID_COMPRADOR
+        ,DATEID                                         AS ID_DATE
+        ,QTYSOLD                                        AS QUANTIDADE_VENDIDA
+        ,PRICEPAID                                      AS VALOR_PAGO
+        ,COMMISSION                                     AS COMISSAO
+        ,TO_CHAR(SALETIME, 'dd/mm/yyyy HH24:MI:SS')     AS DATA_HORA_VENDA
+        ,SALETIME::DATE                                 AS DATA_VENDA
+        ,SALETIME::TIME                                 AS HORA_VENDA
     FROM source_sales
 )
 SELECT * FROM renamed
